c93be16a7016c8c7e52f1c67f7f344c45762ff48
techfreek <alexgbahm@gmail.com>
2013-11-14 00:42:27 -0800

    I decided waiting to implementing a way to append / remove specific files from the library was lame. So now it can parse from an XML file, and it can compare against the list of files so it doesn't need to do a full re-scan each time.

--- Program.cs
+++ Program.cs
@@ -16,8 +16,14 @@ namespace WallpaperManager
     {
         static void Main(string[] args)
         {
+            List<imageNode> parsedLibrary = null;
+            bool previouslyIndexed = false;
             string dir = null;
+
             int i = 0;
+            int height = 0;
+            int width = 0;
+            double ratio = 0;
 
             double singeScreenRatio = 1.8;
             double doubleScreenRatio = 3.6;
@@ -31,11 +37,62 @@ namespace WallpaperManager
 
             var files = Directory.GetFiles(dir);
 
+            var library = from file in files
+                          where file.Contains("wpmLib.xml")
+                          select file;         
+
             List<imageNode> images = new List<imageNode>(files.Count() + 1);
 
-            int height = 0;
-            int width = 0;
-            double ratio = 0;
+            if (library.Count() == 1)
+            {
+                previouslyIndexed = true;
+                Console.WriteLine("Library has already been indexed");
+                parsedLibrary = xmlParse(dir);
+            }
+
+            if (previouslyIndexed)
+            {
+                var removedFiles = from wallpaper in parsedLibrary
+                                   where (!files.Contains(wallpaper.filePath))
+                                   select wallpaper.filePath;
+
+                List<string> libraryFiles = new List<string>();
+
+                foreach(var wallpaper in parsedLibrary)
+                {
+                    libraryFiles.Add(wallpaper.filePath);
+                }
+
+                var addedFiles = from file in files
+                                 where ((!libraryFiles.Contains(file)) && ((file.Contains(".png") || file.Contains(".jpg"))))
+                                 select file;
+
+                if (addedFiles.Count() > 0)
+                {
+                    List<string> aFiles = new List<string>();
+                    foreach (var file in addedFiles)
+                    {
+                        aFiles.Add(file.ToString());
+                    }
+                    addToLib(aFiles, dir);
+                }
+                
+                if (removedFiles.Count() > 0)
+                {
+                    List<string> rFiles = new List<string>();
+                    foreach(var file in removedFiles)
+                    {
+                        rFiles.Add(file.ToString());
+                    }
+                    removeFromLib(rFiles, dir);
+                }
+
+                
+
+                libraryFiles.Clear();
+
+            }
+            
 
             foreach (string img in files)
             {
@@ -81,6 +138,29 @@ namespace WallpaperManager
             xmlExport(images, dir);
         }
 
+        public static List<imageNode> xmlParse(string dirPath)
+        {
+            XDocument library = XDocument.Load(dirPath + "\\" + "\\" + "wpmLib.xml");
+            int i = 0;
+            var wallpapers = from item in library.Descendants("wallpaper")
+                            select new
+                            {
+                                filepath = item.Element("filepath"),
+                                aspectRatio = item.Element("aspectRatio")
+                            };
+            List<imageNode> images = new List<imageNode>(wallpapers.Count());
+
+            foreach (var wallpaper in wallpapers)
+            {
+                imageNode image = new imageNode();
+                image.aspectRatio = Convert.ToDouble(wallpaper.aspectRatio.Value);
+                image.filePath = wallpaper.filepath.Value.ToString();
+                images.Insert(i, image);
+                i++;
+            }
+
+            return images;
+        }
 
         public static bool xmlExport(List<imageNode> images, string dirPath)
         {
@@ -90,8 +170,8 @@ namespace WallpaperManager
             foreach (imageNode image in images)
             {
                 root.Add(new XElement("wallpaper",
-                                new XAttribute("filepath", image.filePath),
-                                new XAttribute("aspectRatio", image.aspectRatio)));
+                                new XElement("filepath", image.filePath),
+                                new XElement("aspectRatio", image.aspectRatio)));
                 
             }
 
@@ -107,7 +187,61 @@ namespace WallpaperManager
             return true;
 
         }
+
+        public static void addToLib(List<string> addedFiles, string dirPath)
+        {
+            string filepath = dirPath + "\\" + "\\" + "wpmLib.xml";
+
+            XDocument library = XDocument.Load(filepath);
+
+            var root = library.Root;
+
+            foreach (string file in addedFiles)
+            {
+                Image currIMG = Image.FromFile(file);
+                double ratio = (double)currIMG.Width / currIMG.Height;
+                ratio = Math.Round(ratio, 2);
+                currIMG.Dispose();
+
+                root.Add(new XElement("wallpaper",
+                                new XElement("filepath", file),
+                                new XElement("aspectRatio", ratio)));
+            }
+            File.Delete(filepath);
+            library.Save(filepath);
+        }
+
+        public static void removeFromLib(List<string> removedFiles, string dirPath)
+        {
+            string filepath = dirPath + "\\" + "\\" + "wpmLib.xml";
+
+            XmlDocument library = new XmlDocument();
+            library.Load(filepath);
+
+            XmlElement root = library.DocumentElement;
+            XmlNodeList wallpapers = root.ChildNodes;
+
+            foreach (string file in removedFiles)
+            {
+                foreach (XmlNode node in wallpapers)
+                {
+                    //Console.WriteLine(node.FirstChild.ToString());
+                    if (node.InnerText.Contains(file))
+                    {
+                        node.ParentNode.RemoveChild(node);
+
+                    }
+                }
+                //string test = String.Format("//wallpaper[@filepath='{0}']", file);
+                //XmlNode node = library.SelectSingleNode(test);
+                
+            }
+            
+            library.Save(filepath);
+        }
     }
+
+    
 
     class imageNode
     {
