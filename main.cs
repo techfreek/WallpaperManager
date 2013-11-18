223a9d09a43297e492e28028dce0b823e18b37b0
techfreek <alexgbahm@gmail.com>
2013-11-17 22:06:29 -0800

    Added comments. I can now read in the current number of monitors, along with storing preference on updating wallpaper while on battery power (actual reading of battery status is a WIP). I further broke portions of the code into functions for greater re-usability. I also realized saving the preferences to program files was frowned upon, so it now saves to appdata. The program now compiles and works. I am researching how I can add support to win8 as System.Drawing.Image is no longer supported. More on this later, I may have to push back win8 support depending on what the consensus shows. That's all I can think of for the moment. May be a couple days before my next update. I have an exam coming up.

--- main.cs
+++ main.cs
@@ -6,137 +6,194 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
 using System.Xml.Linq;
+using System.Windows;
+using System.Windows.Forms;
 
 namespace WallpaperManager
 {
-    class main
+    class manager
     {
-        static void main(string[] args)
+        static void Main(string[] args)
         {
-            List<string> dirs = null;
             string consoleInput = null;
+            bool existingSettings = false;
+            Wallpapers library = null; 
 
             Console.WriteLine("Enter 'exit' at anytime to quit the current operation");
 
+            //Counts how many monitors are currently connected (this is updated live, so I can use this to rescan later, some other methods are constant...)
+            numScreens = System.Windows.Forms.SystemInformation.MonitorCount;
+
+            Console.WriteLine("You have " + numScreens + " screens");
+
+            //Attempts to parse settings. If it is succesful, the app has been configured before
             if (parseSettings())
             {
+                existingSettings = true;
                 Console.WriteLine("Settings loaded");
                 Console.Write("Enter new settings? (Y/N) ");
                 consoleInput = Console.ReadLine();
             }
 
+            //If it hasn't been run before, or the user wants to enter new settings
+            if (!existingSettings || consoleInput.ToLower().Contains("y"))
+            {
+                changeSettings();
+            }
+            
+            //pass the directories and opinion on recursion to the helper class
+            library = new Wallpapers(directories, recursive);
+
+            library.indexer();
+
+            libraryStats(library);
+
+        }
+
+        static void libraryStats(Wallpapers library)
+        {
+            List<string> singleMonitor = library.getImageOfWidth(1);
+            List<string> doubleMonitor = library.getImageOfWidth(2);
+            List<string> tripleMonitor = library.getImageOfWidth(3);
+
+            Console.WriteLine("Single Monitor Wallpapers: " + singleMonitor.Count);
+            Console.WriteLine("Double Monitor Wallpapers: " + doubleMonitor.Count);
+            Console.WriteLine("Triple Monitor Wallpapers: " + tripleMonitor.Count);
+        }
+
+        static void changeSettings()
+        {
+            List<string> dirs = new List<string>();
+            string consoleInput = null;
+
+            do
+            {
+                Console.Write("Enter a directory (absolute path): ");
+                consoleInput = Console.ReadLine();
+                if ((!consoleInput.Contains("exit")) && (Directory.Exists(consoleInput)) && (!dirs.Contains(consoleInput)))
+                { //makes sure we don't enter an exit command, an already existing directory, or a directory that is not on the drive
+                    dirs.Add(consoleInput);
+                }
+            } while (!consoleInput.Contains("exit"));
+
+            directories = dirs;
+
+            Console.Write("Scan recursively? (Y/N)");
+            consoleInput = Console.ReadLine();
+
             if (consoleInput.ToLower().Contains("y"))
             {
-                do
-                {
-                    Console.Write("Enter a directory (absolute path): ");
-                    consoleInput = Console.ReadLine();
-                    if ((!consoleInput.Contains("exit")) && (Directory.Exists(consoleInput)))
-                    {
-                        dirs.Add(consoleInput);
-                    }
-                } while (!consoleInput.Contains("exit"));
-
-                directories = dirs;
-
-                Console.Write("Scan recursively? (Y/N)");
-                consoleInput = Console.ReadLine();
-
-                if (consoleInput.ToLower().Contains("y"))
-                {
-                    recursive = true;
-                }
-                else
-                {
-                    recursive = false;
-                }
-
-                Console.Write("Update Interval? ");
-                consoleInput = Console.ReadLine();
-
-                updateInterval = System.Convert.ToInt32(consoleInput);
-
-                saveSettings(recursive, dirs, updateInterval);
+                recursive = true;
             }
+            else
+            {
+                recursive = false;
+            }
+
+            //Idea borrowed from windows where it can disable on battery
+            Console.Write("Run on battery power? (Y/N)");
+            consoleInput = Console.ReadLine();
+
+            if (consoleInput.ToLower().Contains("y"))
+            {
+                battery = true;
+            }
+            else
+            {
+                battery = false;
+            }
+
+            Console.Write("Update Interval (minutes)? ");
+            consoleInput = Console.ReadLine();
+
+            updateInterval = System.Convert.ToInt32(consoleInput);
+
+            //passes settings on so they can be saved for future use
+            saveSettings();
         }
 
         static bool parseSettings()
         {
-            string settingsPath = programFiles + doubleBackSlash + "settings.xml";
+            string settingsPath = userAppData + doubleBackSlash + "settings.xml";
             if (File.Exists(settingsPath))
             {
                 XDocument settings = XDocument.Load(settingsPath);
+                directories = new List<string>();
 
+                //These can have only 1 value, so they are handled in 1 line. Uses System.Convert._____ to get them from strings to the appropriate datatypes
                 recursive = System.Convert.ToBoolean(settings.Descendants("recursive").Single().Value);
+                updateInterval = System.Convert.ToInt32(settings.Descendants("interval").Single().Value.ToString());
+                battery = System.Convert.ToBoolean(settings.Descendants("runOnBattery").Single().Value);
 
-                var tempDirs = from newDirs in settings.Descendants("directories")
+                //There can be multiple "directory" elements, so they haved to be saved (according to the query syntax) as an 'a variable. I later will need to convert it to a known datatype
+                var tempDirs = from newDir in settings.Descendants("directory")
                 select new
                 {
-                    tempDirs = newDirs.Value
+                    dir = newDir.Value
                 };
 
                 foreach (var tempDir in tempDirs)
                 {
-                    directories.Add(tempDir.ToString());
+                    directories.Add(tempDir.dir.ToString());
                 }
 
-                updateInterval = System.Convert.ToInt32(settings.Descendants("interval").Single().Value.ToString());
+                
                 return true;
             }
+            //If settings don't exist, then the app has not been run before
             return false;             
         }
 
-        static void saveSettings(bool recursion, List<string> newDirectories, int newUpdateInterval)
+        static void saveSettings()
         {
             XDocument settings = null;
-            string settingsPath = programFiles + doubleBackSlash + "settings.xml";
+            string settingsPath = userAppData + doubleBackSlash + "settings.xml";
             if(File.Exists(settingsPath))
-            {
+            { //Currently the algorithm re-writes the file with the new information.
                 settings = XDocument.Load(settingsPath);
-
-                var root = settings.Root;
-
-                root.Add(new XElement("recursive", recursive));
-
-                root.Add(new XElement("directories"));
-
-                var dirTag = root.Element("directories");
-
-                foreach (string directory in newDirectories)
-                {
-                    dirTag.Add(new XElement("directory", directory));
-                }
-
-                root.Add(new XElement("interval", newUpdateInterval));
+                settings.Root.RemoveAll();
             }
             else
-            {
+            { //If the file doesn't exist, it has to be recreated
                 settings = new XDocument(new XElement("settings"));
-                var root = settings.Root;
+            }
 
-                root.Add(new XElement("recursive", recursive));
+            var root = settings.Root;
+               
+            root.Add(new XElement("recursive", recursive));
+            root.Add(new XElement("runOnBattery", battery));
+            root.Add(new XElement("interval", updateInterval));
 
-                root.Add(new XElement("directories"));
+            root.Add(new XElement("directories"));
+            var dirTag = root.Element("directories");
+            foreach (string directory in directories)
+            {
+                dirTag.Add(new XElement("directory", directory));
+            }
 
-                var dirTag = root.Element("directories");
-
-                foreach (string directory in newDirectories)
-                {
-                    dirTag.Add(new XElement("directory", directory));
-                }
-
-                root.Add(new XElement("interval", newUpdateInterval));
+            if (!Directory.Exists(userAppData))
+            { //Verifies that the application folder exists before attempting to save
+                Directory.CreateDirectory(userAppData);
             }
 
             settings.Save(settingsPath);
-            File.SetAttributes(settingsPath, FileAttributes.Hidden);
+
         }
 
+        //Saved variables
         public static bool recursive { get; set; }
+        public static bool battery { get; set; }
         public static List<string> directories { get; set; }
         public static int updateInterval { get; set; }
+
+        //Constantly updated variables
+        public static bool ACPower { get; set; }
+        public static int numScreens { get; set; }
+
+        //Static variables that would be redundant to export
         public static string doubleBackSlash = "\\" + "\\";
-        public static string programFiles = "C:" + doubleBackSlash + "Program Files (x86)" + doubleBackSlash + "Wallpaper Manager";
+        public static string userAppData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + doubleBackSlash + "Wallpaper Manager";
+        public static string commonAppData = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData) + "Wallpaper Manager";
         
     }
 }
--- wallpapers.cs
+++ wallpapers.cs
@@ -14,40 +14,59 @@ namespace WallpaperManager
 {
     class Wallpapers
     {
-        public static void indexer(List<string> directories)
+        public Wallpapers(List<string> directories, bool recursion)
+        { //Sets initial values, starts allocating memory for various data
+            libPath = directories;
+            recursiveImport = recursion;
+            imageLib = new List<List<imageNode>>();
+        }
+
+        public Wallpapers()
         {
-            List<List<imageNode>> parsedLibrary = null;
-            List<bool> previouslyIndexed = null;
+            libPath.Clear();
+            imageLib.Clear();
+        }
 
-            List<List<string>> files = new List<List<string>>(directories.Count);
+        public void indexer()
+        {
+            List<List<imageNode>> parsedLibrary = new List<List<imageNode>>();
+            List<bool> previouslyIndexed = new List<bool>();
 
-            for (int i = 0; i < directories.Count; i++)
+            List<List<string>> files = new List<List<string>>();
+
+            //I had some trouble dealing with these lists, so I ended up needing to allocate the memory inside each value of the list
+            for (int i = 0; i < libPath.Count; i++)
             {
-                files[i] = getFiles(directories[i]);
+                files.Add(new List<string>());
+                parsedLibrary.Add(new List<imageNode>());
+                previouslyIndexed.Add(new bool());
+                imageLib.Add(new List<imageNode>());
+            }
 
-                if (files[i].Contains("wpmLib.xml"))
+            //I am utilizing for loops due to the number of lists I am using
+            for (int i = 0; i < libPath.Count; i++)
+            {
+                //Gives the filename of the library, so we don't have to scan inside each filename
+                string libFilePath = libPath[i] + "\\" + "wpmLib.xml";
+                files[i].AddRange(getFiles(libPath[i]));
+
+                if (files[i].Contains(libFilePath))
                     previouslyIndexed[i] = true;
                 else
                     previouslyIndexed[i] = false;
-
-                i++;
             }
 
-            for (int i = 0; i < directories.Count; i++)
+            for (int i = 0; i < libPath.Count(); i++)
             {
-                imageLib[i] = new List<imageNode>(files[i].Count() + 1);
-            }
-
-            for (int i = 0; i < directories.Count(); i++)
-            {
-                if (previouslyIndexed.Count() == 0)
+                if (!previouslyIndexed[i])
                 {
-                    newLibrary(directories[i], i);
+                    newLibrary(libPath[i], i);
                 }
                 else
                 {
-                    parsedLibrary[i] = xmlParse(directories[i]);
+                    parsedLibrary[i] = xmlParse(libPath[i]);
 
+                    //Algorithm: looks at every inputted filepath, and if the scanned files doesn't contain that file path, add it to a list
                     var removedFiles = from wallpaper in parsedLibrary[i]
                                        where (!files[i].Contains(wallpaper.filePath))
                                        select wallpaper.filePath;
@@ -55,22 +74,23 @@ namespace WallpaperManager
                     List<string> libraryFiles = new List<string>();
 
                     foreach (var wallpaper in parsedLibrary[i])
-                    {
+                    { //Since I am only using filenames, I have to copy them out of the image files i parsed. Using the parsedLibrary[i] became difficult when I had to find the added files
                         libraryFiles.Add(wallpaper.filePath);
                     }
 
+                    //Algorithm: Looks at every image file that has been scanned in, and not inputted into the library
                     var addedFiles = from file in files[i]
                                      where ((!libraryFiles.Contains(file)) && ((file.Contains(".png") || file.Contains(".jpg"))))
                                      select file;
 
                     if (addedFiles.Count() > 0)
-                    {
+                    {   //addedFiles is a temporary var, so I needed to convert it to a more permanent variable so I could add it to the library
                         List<string> aFiles = new List<string>();
                         foreach (var file in addedFiles)
                         {
                             aFiles.Add(file.ToString());
                         }
-                        addToLib(aFiles, directories[i]);
+                        addToLib(aFiles, libPath[i]);
                     }
 
                     if (removedFiles.Count() > 0)
@@ -80,40 +100,28 @@ namespace WallpaperManager
                         {
                             rFiles.Add(file.ToString());
                         }
-                        removeFromLib(rFiles, directories[i]);
+                        removeFromLib(rFiles, libPath[i]);
                     }
 
+                    //Remove unneeded data
                     libraryFiles.Clear();
 
+                    imageLib[i] = parsedLibrary[i];
                 }
             }
         }
 
         private static void newLibrary(string dirPath, int i)
         {
-            string[] files = Directory.GetFiles(dirPath);
-            int height = 0,
-                width = 0;
-            double ratio = 0.0;
-            imageLib[i] = new List<imageNode>(files[i].Count() + 1);
+            string[] files = Directory.GetFiles(dirPath); //GetFiles only exports to string[] :(
+            imageLib[i] = new List<imageNode>();
 
             foreach (string img in files)
             {
                 if (img.Contains(".png") || img.Contains(".jpg"))
                 {
-                    imageNode newImage = new imageNode();
-                    Image currIMG = Image.FromFile(img);
-
-                    height = currIMG.Height;
-                    width = currIMG.Width;
-                    ratio = (double)width / height;
-                    ratio = Math.Round(ratio, 2);
-
-                    newImage.filePath = img;
-                    newImage.aspectRatio = ratio;
-                    imageLib[i].Insert(imageLib.Count, newImage);
-                    currIMG.Dispose();
-
+                    imageNode newImage = getEXIF(img);
+                    imageLib[i].Add(newImage);
                 }
             }
 
@@ -122,23 +130,26 @@ namespace WallpaperManager
 
         private static List<imageNode> xmlParse(string dirPath)
         {
+            //File has already proven to exist, don't need to double check existance
             XDocument library = XDocument.Load(dirPath + libraryPath);
-            int i = 0;
+
+            //Pulls in raw data, still haven't figured out how to parse directly to a different datatype
             var wallpapers = from item in library.Descendants("wallpaper")
                              select new
                              {
                                  filepath = item.Element("filepath"),
                                  aspectRatio = item.Element("aspectRatio")
                              };
-            List<imageNode> images = new List<imageNode>(wallpapers.Count());
+
+            //Not saved directly to imageLib as more processing is needed
+            List<imageNode> images = new List<imageNode>();
 
             foreach (var wallpaper in wallpapers)
             {
                 imageNode image = new imageNode();
                 image.aspectRatio = Convert.ToDouble(wallpaper.aspectRatio.Value);
                 image.filePath = wallpaper.filepath.Value.ToString();
-                images.Insert(i, image);
-                i++;
+                images.Add(image);
             }
 
             return images;
@@ -146,6 +157,7 @@ namespace WallpaperManager
 
         private static bool xmlExport(List<imageNode> images, string dirPath)
         {
+            //1 library per root folder, so exported individually (at least to avoid confusion)
             XDocument lib = new XDocument(new XElement("wallpapers"));
             var root = lib.Root;
 
@@ -154,23 +166,24 @@ namespace WallpaperManager
                 root.Add(new XElement("wallpaper",
                                 new XElement("filepath", image.filePath),
                                 new XElement("aspectRatio", image.aspectRatio)));
-
             }
 
 
             string libPath = dirPath + libraryPath;
 
+            //Working on a better method of overwriting
             if (File.Exists(libPath))
                 File.Delete(libPath);
 
             lib.Save(libPath);
-
+            
+            //Hides the file from power users
             File.SetAttributes(libPath, FileAttributes.Hidden);
             return true;
 
         }
 
-        private static void addToLib(List<string> addedFiles, string dirPath)
+        private static void addToLib(List<string> addedFiles, string dirPath, int libIndex)
         {
             string filepath = dirPath + libraryPath;
 
@@ -180,20 +193,23 @@ namespace WallpaperManager
 
             foreach (string file in addedFiles)
             {
-                Image currIMG = Image.FromFile(file);
-                double ratio = (double)currIMG.Width / currIMG.Height;
-                ratio = Math.Round(ratio, 2);
-                currIMG.Dispose();
+                //Don't need to save to imageNode as the data would have to be extracted it directly
+                imageNode currIMG = getEXIF(file);
 
+                //Add to image library to avoid reparsing
+                imageLib[libIndex].Add(currIMG);
+
+                //Add to XML file
                 root.Add(new XElement("wallpaper",
-                                new XElement("filepath", file),
-                                new XElement("aspectRatio", ratio)));
+                                new XElement("filepath", currIMG.filePath),
+                                new XElement("aspectRatio", currIMG.aspectRatio)));
             }
+            //overwriting operations
             File.Delete(filepath);
             library.Save(filepath);
         }
 
-        private static void removeFromLib(List<string> removedFiles, string dirPath)
+        private static void removeFromLib(List<string> removedFiles, string dirPath, int libIndex)
         {
             string filepath = dirPath + libraryPath;
 
@@ -207,11 +223,21 @@ namespace WallpaperManager
             {
                 foreach (XmlNode node in wallpapers)
                 {
-                    //Console.WriteLine(node.FirstChild.ToString());
+                    //Checks all of the nodes to see if they contain the same filepath, if so, it is deleted.
+                    //I intend to re-write this to minimize the time complexity here which is usually greater than removedFiles^2
                     if (node.InnerText.Contains(file))
                     {
                         node.ParentNode.RemoveChild(node);
+                        break; //Cuts down on processing. There should not be duplicates
 
+                    }
+                }
+                foreach (imageNode image in imageLib[libIndex])
+                {
+                    if (image.filePath.Equals(file))
+                    {
+                        imageLib[libIndex].Remove(image);
+                        break; //Cuts down on processing. There should not be two files of the same name
                     }
                 }
             }
@@ -219,27 +245,50 @@ namespace WallpaperManager
             library.Save(filepath);
         }
 
-        public List<imageNode> getImage(int screenWidth)
+        //Gets a list of wallpapers that match a specified number of monitors
+        //Designed on the notion that most people use 16:9/16:10 monitors. Wider support will come later.
+        public List<string> getImageOfWidth(int screenWidth)
         {
-            List<imageNode> images = null;
-            double ratio = 1.8;  //calculations have shown the largest ratio is 1.8 between width and height.
+            List<string> images = new List<string>();
+            double ratio = 1.8, //calculations have shown the largest ratio is 1.8 between width and height.
+                min = ratio * (screenWidth - 1),
+                max = ratio * screenWidth;
 
-            for (int i = 0; i < imageLib.Count; i++)
+            //I do not have a single unified library, so I must iterate through the individual libraries.
+            for (int i = 0; i < imageLib.Count; i++) //using loops instead of queries because I have a list of libraries and that is harder to query
             {
                 foreach(imageNode image in imageLib[i])
                 {
-                    if (((ratio * screenWidth) <= image.aspectRatio) && (image.aspectRatio < ((ratio + 1) * screenWidth)))
+                    //Checks to make sure the image is wider (monitors - 1), and narrower than (monitors + 1)
+                    if ((min <= image.aspectRatio) && (image.aspectRatio < max))
                     {
-                        images.Add(image);
+                        images.Add(image.filePath);
                     }
                 }
             }
             return images;
         }
 
-        private static List<string> getFiles(string directory)
+        private static imageNode getEXIF(string filename)
         {
-            List<string> files = null;
+            //A few functions call on this, makes it easier than updating code in a few places. Also will be easier when I add win8 support
+            imageNode newImage = new imageNode();
+            Image currIMG = Image.FromFile(filename); //According to initial research, Image in this format is not supported in Win8, need to find new method
+
+            int height = currIMG.Height;
+            int width = currIMG.Width;
+            double ratio = (double)width / height;
+            ratio = Math.Round(ratio, 2); //Accuracy is not too crucial here
+
+            newImage.filePath = filename;
+            newImage.aspectRatio = ratio;
+            currIMG.Dispose(); //Free up memory
+            return newImage;
+        }
+
+        private static List<string> getFiles(string directory)
+        { //Implemented to get files recursively/non-recursively easier. It also converts it to a list which is currently my preferred method for accessing the filenames
+            List<string> files = new List<string>();
             List<string> directories = null;
             string[] tempFiles = null;
             string[] tempDir = null;
@@ -250,13 +299,16 @@ namespace WallpaperManager
                 directories = tempDir.ToList();
                 foreach (string dir in directories)
                 {
+                    //Each getFiles returns a list of all the files, and we can merge to the existing list with AddRange
                     files.AddRange(getFiles(dir));
                 }
+                //Add local files to list
                 tempFiles = Directory.GetFiles(directory);
                 files.AddRange(tempFiles.ToList());
             }
             else
             {
+                //Otherwise only local files are returned
                 tempFiles = Directory.GetFiles(directory);
                 files = tempFiles.ToList();
             }
@@ -264,7 +316,10 @@ namespace WallpaperManager
             return files;
         }
 
-        private static string libraryPath = "\\" + "\\" + "wpmLib.xml";
+        //Used in many operations when using it. Made sense to write it once instead of re-writing each time
+        private static string libraryPath =  "\\" + "wpmLib.xml";
+
+        //Variables used in the general operation of the program
         public static bool recursiveImport { get; set; }
         public static List<string> libPath { get; set; }
         public static List<List<imageNode>> imageLib { get; set; }
